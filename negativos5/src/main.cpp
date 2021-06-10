15bc4bcbbe7a4cb1b8dd46124007b3b8bf0ea27a
Rafael Silva Freire <rafaelsilvafreire2@gamil.com>
2021-06-10 14:58:33 -0300

    Feito negativos5

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,7 +6,16 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int numbers[5];
+    int negatives = 0;
+
+    for (int i=0; i<5; i++) {
+        cin >> numbers[i];
+        if (numbers[i] < 0)
+            negatives++;
+    }
+
+    cout << negatives;
 
     return 0;
 }
